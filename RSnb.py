8a02b2bd1512febe3d960725e65d7e25507ff2a6
Etietop Udofia <54590798+dgorgo@users.noreply.github.com>
2025-03-24 09:39:39 +0100

    Update RSnb.py

--- RSnb.py
+++ RSnb.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from mlxtend.frequent_patterns import apriori, association_rules
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import cross_validate
